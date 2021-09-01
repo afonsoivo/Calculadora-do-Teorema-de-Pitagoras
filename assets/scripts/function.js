07e8613a0de3c412adf3d1d707c066149ef8c10a
Afonso Ivo <afonso.ivo@gmail.com>
2021-09-01 19:58:48 -0300

    Erro no scripit cat não podem ser maior que a hip

--- assets/scripts/function.js
+++ assets/scripts/function.js
@@ -8,7 +8,7 @@ function calculate(catOp, catAdj, hip) {
     if (!hip) {
         hip = Math.sqrt(Math.pow(catAdj, 2) + Math.pow(catOp, 2))
         document.getElementById('answer').textContent = `Resposta: O valor da hipotenusa é ${hip.toFixed(2)}`
-    } else if (!catOp){
+    } else if (!catOp) {
         catOp = Math.sqrt(Math.pow(hip, 2) - Math.pow(catAdj, 2))
         document.getElementById('answer').textContent = `Resposta: O valor do cateto oposto é ${catOp.toFixed(2)}`
     } else {
@@ -29,12 +29,14 @@ function validation(event) {
     hip = form.elements['hip'].value || null
     if (catOp && catAdj && hip) {
         document.getElementById('answer').textContent = `Por favor digite apenas dois valores!`
-    } else if (!catOp && !hip || !catAdj && !hip || !catAdj && !catOp){
+    } else if (!catOp && !hip || !catAdj && !hip || !catAdj && !catOp) {
         document.getElementById('answer').textContent = `Por favor digite apenas dois valores!`
     } else if (catOp && catOp <= 0 || catAdj && catAdj <= 0 || hip && hip <= 0) {
         document.getElementById('answer').textContent = `Os valores não podem ser negativos ou igual a zero.`
     } else if (isNaN(catOp) || isNaN(catAdj) || isNaN(hip)) {
         document.getElementById('answer').textContent = `Os valores precisam ser números.`
+    } else if (catOp >= hip || catAdj >= hip) {
+        document.getElementById('answer').textContent = `Os Catetos não podem ser maior ou igual a hipotenunsa.`
     } else {
         calculate(catOp, catAdj, hip)
     }
